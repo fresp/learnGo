0b5b183aa97a5fdbcc3a72709244900e812a035e
Freza Nugraha <frezanugraha404@gmail.com>
2019-09-22 01:53:08 +0700

    data type pratice

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ My repo to learning golang
 
 
 1. main.go = build Traditionally the first program "hello world" with go
+2. datatype.go =  practice using types [number, string, bolean]
---
+++ datatype.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//number
+	fmt.Println(1)   //int ype
+	fmt.Println(1.1) //float type
+
+	// string
+	fmt.Println("freza")
+	fmt.Println("freza"[1])
+	fmt.Println("freza " + "nugraha")
+
+	// bolean
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
